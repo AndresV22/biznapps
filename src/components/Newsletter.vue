efb6df8b1e35971fabf462c87b3f54ba3ee7121c
Andres <andres.alcaino@hfsolutions.cl>
2023-03-25 20:57:20 -0300

    añade envio de correo

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -7,7 +7,7 @@
           <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">¡Déjanos tu correo y te contactamos!</h2>
           <p class="mt-4 text-lg leading-8 text-gray-300">O bien mandanos un correo a contacto@biznapps.com</p>
           <div class="mt-6 flex max-w-md gap-x-4">
-            <form @submit.prevent="onSubmit">
+            <form ref="emailForm" @submit.prevent="onSubmit">
               <label for="email-address" class="sr-only">Correo</label>
               <input v-model="form.email" id="email-address" name="email" type="email" autocomplete="email" required="" class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Ingresa tu email" />
               <button type="submit" class="flex-none rounded-md bg-indigo-500 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Suscribirme</button>
@@ -48,6 +48,7 @@
 import { createEmail } from '@/firebase'
 import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/vue/24/outline'
 import { reactive } from 'vue'
+import emailjs from 'emailjs-com'
 
 export default {
   name: 'Newsletter',
@@ -55,19 +56,29 @@ export default {
     CalendarDaysIcon,
     HandRaisedIcon
   },
+  mounted() {
+    emailjs.init('59QH7487ZAcWXO0r5');
+    
+    // enlaza manualmente la función onSubmit al componente
+    this.onSubmit = this.onSubmit.bind(this);
+  },
   setup() {
     const form = reactive({
       email: ''
     })
 
-    const onSubmit = async () => {
+    const onSubmit = async (e) => {
       try {
         await createEmail(form.email)
+        try {
+          await emailjs.sendForm('service_99f3jwq', 'template_zaigpxb', e.target,
+          '59QH7487ZAcWXO0r5')
+        } catch (error) {
+          console.log({ error })
+        }
         form.email = ''
-        console.log("Paso por aqui");
       } catch (error) {
         console.log(error)
-        console.log("Paso por aqui");
       }
     }
 
